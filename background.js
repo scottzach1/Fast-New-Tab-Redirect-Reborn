4bec21dc7b9861ee2c3f7c44dfa704bf3a8aa1bd
Zac Scott <scottzach1@myvuw.ac.nz>
2020-05-30 18:38:05 +1200

    Initialises project from existing extension files.
    https://chrome.google.com/webstore/detail/fast-new-tab-redirect/ohnfdmfkceojnmepofncbddpdicdjcoi?hl=en

---
+++ background.js
@@ -0,0 +1,7 @@
+chrome.webRequest.onBeforeRequest.addListener((function() {
+  return {
+    redirectUrl: localStorage.redirectUrl || chrome.extension.getURL("options.html")
+  };
+}), {
+  urls: [chrome.extension.getURL("options.html")]
+}, ["blocking"]);
---
+++ options.js
@@ -0,0 +1,6 @@
+var inputRedirectUrl = document.querySelector("input");
+inputRedirectUrl.value = localStorage.redirectUrl || "";
+
+document.querySelector("form").addEventListener("submit", function() {
+	localStorage.redirectUrl = inputRedirectUrl.value;
+}, false);
